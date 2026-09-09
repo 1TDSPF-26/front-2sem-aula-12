83cd471c7af0abd5ed0bb84f4319bd0316aecdaa
AndrewRls <andrl57adf@gmail.com>
2026-09-09 10:49:04 -0300

    feat: menu de navegação criado

---
+++ my-app/src/components/Menu/Menu.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+export default function Menu() {
+    return (
+
+        <nav>
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/produtos'>Produtos</Link></li>
+                <li><Link to='/editar-produtos/10'>Editar Produtos</Link></li>
+            </ul>
+        </nav>
+    )
+}
