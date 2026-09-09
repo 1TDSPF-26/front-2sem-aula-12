2a2537ad31061c5488538f4a1f10982a621ab38f
MatheusRodriguesSerrao <rmatheusrodrigues193@gmail.com>
2026-09-09 12:29:05 -0300

    feat: Componente ObservadorDeRota adiocionado para monitorar navegação no App.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router";
 import Cabecalho from "./components/Cabecalho/Cabecalho";
-import Conteudo from "./components/Conteudo/Conteudo";
 import Rodape from "./components/Rodape/Rodape";
+import ObservadorDeRota from "./components/ObservadorDeRota/ObservadorDeRota";
 
 export default function App() {
   return (
     <div>
+      <ObservadorDeRota/>
       <Cabecalho/>
       <Outlet/>
       <Rodape/>
